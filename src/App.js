622b2df85c18fbf6f244575c2398b64aa28502ed
Michael Hall <mohall42@students.tntech.edu>
2019-08-10 14:13:27 -0400

    Working on api call

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React, {Component}  from 'react'
 import Conditional from './components/Conditional'
 
 
+
+const apiKey = process.env.REACT_APP_SPOONFULL_API_KEY;
+
 class App extends Component {
 
-  constructor(){
-    super()
+    constructor(){
+      super()
 
-    this.state = {
-      isLoading: true
+      this.state = {
+        isLoading: true,
+        recipe: {},
+        recipes: []
+      }
+
     }
 
-  }
   
   componentDidMount(){
-    fetch("https://api.spoonacular.com/recipes/random?number=1")
+    fetch("https://api.spoonacular.com/recipes/random?number=10", {
+       headers: {
+         "Api-Key": {apiKey},
+       }
+    })
     .then(Response => console.log(Response))
     
 
