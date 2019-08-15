875b00dafb9549706d1c0359ac9ce9bfb91f29e8
Michael Hall <mohall42@students.tntech.edu>
2019-08-14 23:54:42 -0400

    Added routing package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, {Component}  from 'react'
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import styled from 'styled-components'
 import background from './images/Background.svg'
+import LandingPage from './components/LandingPage'
 import Ingredient from './components/Ingredient'
 import PickPage from './components/PickPage'
 
@@ -26,68 +28,19 @@ overflow: hidden;
 
 `;
 
-class App extends Component {
+function App() {
+  return(
 
-    constructor(){
-      super()
-
-      this.state = {
-        ingredients: ''
-      };
-
-    
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-
-    }
-
-    handleSubmit(event){
-      alert("Entered Ingredients " + this.state.ingredients)
-      event.preventDefault();
-      
-    }
-
-    handleChange(event){
-      this.setState({
-        ingredients: event.target.value
-      })
-    }
-
-    
-  render(){
-
-      
-    
-
-      
-
-    return (
+    <Router>
       <Main>
-        
-         <Box>
+        <LandingPage />
 
-          <form onSubmit = {this.handleSubmit}>
-            <h1>Please enter ingredients</h1>
-            <input type="text"
-              placeholder="Ingredients"
-              name="Ingredient"
-              value={this.state.ingredients}
-              onChange={this.handleChange}
-
-              />
-
-              <input type= "submit"   value="Submit" />
-        
-          </form>
-
-
-         </Box>
-
-        
-        
       </Main>
-    )
-  }
+
+    </Router>
+  )
+
+    
 }
 
 export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,101 @@
+import React, {Component}  from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import styled from 'styled-components'
+import background from '../images/Background.svg'
+import PickPage from './PickPage'
+
+const Box = styled.div`
+  
+  display: flex;
+  height: 500px;
+  width: 1000px;
+  margin: 10% auto;
+  background: white;
+
+`;
+
+const Main = styled.div`
+
+
+margin: 0;
+height: 100vh;
+width: 100vw;
+background: url(${background}) no-repeat;
+background-size: cover;
+overflow: hidden;
+
+`;
+
+class LandingPage extends Component {
+
+    constructor(){
+      super()
+
+      this.state = {
+        ingredients: '',
+        submitArray:[]
+      };
+
+    
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleSubmit(event){
+      
+      this.setState({
+        submitArray: this.state.ingredients.split(' ')
+      })
+
+      alert(this.state.submitArray)
+
+  
+      event.preventDefault();
+      
+    }
+
+    handleChange(event){
+      this.setState({
+        ingredients: event.target.value
+      })
+    }
+
+    
+  render(){
+
+      
+    
+
+      
+
+    return (
+      <Main>
+        
+         <Box>
+
+          <form >
+            <h1>Please enter ingredients</h1>
+            <input type="text"
+              placeholder="Ingredients"
+              name="Ingredient"
+              value={this.state.ingredients}
+              onChange={this.handleChange}
+
+              />
+
+              <button type= "Submit" onClick={this.handleSubmit} >Submit</button>
+        
+          </form>
+
+
+         </Box>
+
+        
+        
+      </Main>
+    )
+  }
+}
+
+export default LandingPage;
